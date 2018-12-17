d210ea1d6e49f68c36319f056fcfb217c8a652dc
吴波 <913721086@qq.com>
2018-12-17 10:08:56 +0800

    .

--- lib/net/_axios.d.ts
+++ lib/net/_axios.d.ts
@@ -1,2 +1,15 @@
-export declare const __MyInstance: import("axios").AxiosInstance;
+import { AxiosInstance } from "axios";
+export declare const enum CT {
+    URLENCODED = "application/x-www-form-urlencoded",
+    JSON = "application/json"
+}
+export interface AAOPTS {
+    ct: CT;
+}
+export declare class CAxios {
+    private instance;
+    constructor(opts: AAOPTS);
+    get_inst(): AxiosInstance;
+}
+declare const __MyInstance: AxiosInstance;
 export default __MyInstance;
