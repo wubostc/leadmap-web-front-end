4bb4cf7e6dfc4b44f127cb3d40151b3b1e33ca92
吴波 <913721086@qq.com>
2018-12-17 14:45:18 +0800

    .

--- lib/net/_axios.d.ts
+++ lib/net/_axios.d.ts
@@ -1,7 +1,7 @@
-import { AxiosInstance } from "axios";
+import { AxiosRequestConfig, AxiosInstance } from "axios";
 export declare const enum CT {
-    URLENCODED = "application/x-www-form-urlencoded",
-    JSON = "application/json"
+    URLENCODED = 0,
+    JSON = 1
 }
 export interface AAOPTS {
     ct: CT;
@@ -9,7 +9,10 @@ export interface AAOPTS {
 export declare class CAxios {
     private instance;
     constructor(opts: AAOPTS);
+    protected request(config: AxiosRequestConfig): AxiosRequestConfig;
+    protected request1(config: AxiosRequestConfig): AxiosRequestConfig;
     get_inst(): AxiosInstance;
+    protected static set_auth(config: AxiosRequestConfig): void;
 }
 declare const axios: AxiosInstance;
 export { axios };
